b9098136f5b40a0f238e7af29e2e8ed06143eb4a
taqi <taqiu@example.com>
2024-10-09 03:35:47 -0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,18 @@ export default function RootLayout({
         {/* Header */}
         <header className="bg-blue-500 flex items-center justify-between p-4 text-white fixed top-0 w-full z-10">
           <div className="flex items-center">
-            <img src="https://i.pinimg.com/736x/33/85/ba/3385ba649152c101df1a13b21241c3bd.jpg" className="rounded-3xl h-10 w-auto mr-4" alt="" />
-            <h1 className="text-xl font-bold">SAMI'S Blogging Website</h1>
+            <img 
+              src="https://i.pinimg.com/736x/33/85/ba/3385ba649152c101df1a13b21241c3bd.jpg" 
+              className="rounded-3xl h-10 w-auto mr-4" 
+              alt="" 
+            />
+            <h1 className="text-xl font-bold">SAMI&apos;S Blogging Website</h1>
           </div>
-          <img src="https://images.ctfassets.net/23aumh6u8s0i/6pjUKboBuFLvCKkE3esaFA/5f2101d6d2add5c615db5e98a553fc44/nextjs.jpeg" className="rounded-3xl h-10 w-auto" alt="Right Image" />
+          <img 
+            src="https://images.ctfassets.net/23aumh6u8s0i/6pjUKboBuFLvCKkE3esaFA/5f2101d6d2add5c615db5e98a553fc44/nextjs.jpeg" 
+            className="rounded-3xl h-10 w-auto" 
+            alt="Right Image" 
+          />
         </header>
 
         {/* Main Content */}
